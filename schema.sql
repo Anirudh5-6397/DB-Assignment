521d481c46c8f72cee66fcf946ce0531892daa02
Anirudh <92909313+Anirudh5-6397@users.noreply.github.com>
2024-03-11 00:54:18 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,50 @@
+Create Product table:-
+CREATE TABLE Product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    SKU VARCHAR(255),
+    category_id INT,
+    inventory_id INT,
+    price DECIMAL(10,2),
+    discount_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES Product_Category(id),
+    FOREIGN KEY (inventory_id) REFERENCES Product_Inventory(id),
+    FOREIGN KEY (discount_id) REFERENCES Discount(id)
+);
+
+Create Product_Category table:-
+CREATE TABLE Product_Category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+
+Create Product_Inventory table:-
+CREATE TABLE Product_Inventory (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    quantity INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+ Create Discount table:-
+CREATE TABLE Discount (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    discount_percent DECIMAL(5,2),
+    active BOOLEAN,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+ 
